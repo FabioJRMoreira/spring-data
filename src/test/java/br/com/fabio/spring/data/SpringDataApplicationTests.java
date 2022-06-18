026acbe398321d814508b88cb128497011838bdc
client <client@192.168.2.29>
2022-06-18 08:41:44 -0400

    primeiro comit do projeto

---
+++ src/test/java/br/com/fabio/spring/data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabio.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
